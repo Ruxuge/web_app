5dc74c26efd672a38b49ce1f76604ce9fa51c760
OskarPrzydatek <o.przydatek@wp.pl>
2020-11-29 20:16:25 +0100

    first color setup

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.scss';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>I'm working!</h1>
     </div>
   );
 }
-
-export default App;
